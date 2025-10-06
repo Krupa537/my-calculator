950941325a6d426fa81c39781071b84db3cae2c2
Krupa537 <krupaprakash3@gmail.com>
2025-10-06 12:29:32 +0530

    Update test_calculator.py

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -18,16 +18,31 @@ class TestBasicOperations:
         assert subtract(5, 3) == 2
         assert subtract(10, 4) == 6
 
-        
-    def test_multiply_negative_numbers(self) :
-            """test multiplying negative numbers"""
-            assert multiply(-2 , 3) ==-6
-            assert multiply(-4 , -5) ==20
+    def test_subtract_negative_numbers(self):
+        """Test subtracting negative numbers"""
+        assert subtract(-5, -3) == -2
+        assert subtract(-10, -4) == -6
 
-def test_divide_positive_numbers(self) :
-    """test dividing positive numbers"""
-    assert divide(10 ,2) ==5
-    assert divide(15 ,3) ==5
+        
+    def test_multiply_by_zero(self):
+        """Test multiplying by zero """
+        assert multiply(5,0) == 0
+        assert multiply(0,10) == 0
+
+    def test_multiply_negative_numbers(self):
+        """Test adding positive numbers"""
+        assert multiply(-2, 3) == 6
+        assert multiply(-4, -5) == 20
+    
+    def test_divide_positive_numbers(self):
+        """Test dividing positive numbers"""
+        assert divide(10, 3) == 5
+        assert divide(15, 3) == 5
+
+   def test_divide_negative_numbers(self):
+        """Test dividing negtive numbers"""
+        assert divide(-10, 3) == -5
+        assert divide(-12, -3) == 4
     
     
 
